043d03ef781764a99766cb73ff60a4c1d4936a99
Per Dalsten Sorensen <pdalsten@gmail.com>
2018-01-09 18:24:00 -0500

    Added main class

---
+++ src/main/java/dk/purplegreen/musiclibrary/tools/MusicLibraryTools.java
@@ -0,0 +1,37 @@
+package dk.purplegreen.musiclibrary.tools;
+
+public class MusicLibraryTools {
+
+	public static void main(String[] args) {
+		if (!(args.length > 0)) {
+			showHelp();
+		} else {
+			switch (args[0]) {
+			case "derbyimport":
+				DerbyImport.main(args);
+				break;
+			case "derbyexport":
+				DerbyExport.main(args);
+				break;
+			case "msyqlexport":
+				MySQLExport.main(args);
+				break;
+			case "audiotagimport":
+				AudioTagImport.main(args);
+				break;
+			case "help":
+			default:
+				showHelp();
+			}
+		}
+	}
+
+	private static void showHelp() {
+		System.out.println("Usage: derbyimport|derbyexport|mysqlexport|audiotagimport");
+		System.out.println("\n\tderbyimport: Import from Derby to XML");
+		System.out.println("\tderbyexport: Export from XML to Derby");
+		System.out.println("\tmysqlexport: Export from XML to MySQL");
+		System.out.println("\taudiotagimport: Import from audio tagged MP3 to XML");
+	}
+
+}
